8710218e4aedba085df706af788f1d7699d9da41
Jan Adamíra <jan.adamira@softgate.cz>
2025-12-03 14:26:48 +0100

    Added day3 part1

---
+++ AdventOfCode2025/Day3.cs
@@ -0,0 +1,48 @@
+﻿namespace AdventOfCode2025
+{
+    public static class Day3
+    {
+        public static string InputFile = "../../../Inputs/Day3Input.txt";
+        public static void Run()
+        {
+            Console.WriteLine("Running Day 3 Challenge...");
+            Console.WriteLine(Part1());
+            Console.WriteLine(Part2());
+            Console.WriteLine("Day 3 Challenge Complete.");
+        }
+        public static string Part1()
+        {
+            var finalSum = 0;
+            foreach (var line in File.ReadAllLines(InputFile))
+            {
+                var numbers = line.Select(c => c.ToString()).ToArray();
+                int[] ints = new int[numbers.Length];
+                for (int i = 0; i < ints.Length; i++)
+                {
+                    ints[i] = int.Parse(numbers[i]);
+                }
+                int largest = 0;
+                int secondLargest = ints[^1];
+                for (int i = 0; i < ints.Length - 1; i++)
+                {
+                    if (ints[i] > largest)
+                    {
+                        secondLargest = ints[^1];
+                        largest = ints[i];
+                    }
+                    else if (ints[i] > secondLargest)
+                    {
+                        secondLargest = ints[i];
+                    }
+                }
+                finalSum += (largest * 10) + secondLargest;
+            }
+            return "Part 1 Solution: " + finalSum;
+        }
+        public static string Part2()
+        {
+            // Implement Part 2 logic here
+            return "Part 2 Solution: TBD";
+        }
+    }
+}
--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -5,5 +5,6 @@
         Console.WriteLine("Advent of Code 2025");
         AdventOfCode2025.Day1.Run();
         AdventOfCode2025.Day2.Run();
+        AdventOfCode2025.Day3.Run();
     }
 }
