6e813541ccc7cc968f53200763f72196df5a4738
Jan Adamíra <jan.adamira@softgate.cz>
2025-12-04 15:28:05 +0100

    Day 4

--- AdventOfCode2025/Day4.cs
+++ AdventOfCode2025/Day4.cs
@@ -12,13 +12,98 @@
         }
         public static string Part1()
         {
-
-            return "Part 1 Solution: [Not Implemented]";
+            char[][] grid = File.ReadAllLines(InputFile)
+                .Select(line => line.ToCharArray())
+                .ToArray();
+            var deletable = 0;
+            for (int r = 0; r < grid.Length; r++)
+            {
+                for (int c = 0; c < grid[r].Length; c++)
+                {
+                    if (grid[r][c] == '@')
+                    {
+                        if (IsDeletable(grid, r, c))
+                        {
+                            deletable++;
+                            //grid[r][c] = 'x';
+                        }
+                    }
+                }
+            }
+            return "Part 1 Solution: " + deletable; ;
         }
+
+
         public static string Part2()
         {
-            // Placeholder for Part 2 logic
-            return "Part 2 Solution: [Not Implemented]";
+            char[][] grid = File.ReadAllLines(InputFile)
+            .Select(line => line.ToCharArray())
+            .ToArray();
+            var deletable = 0;
+            var lastDeleted = 0;
+            do
+            {
+                lastDeleted = 0;
+                for (int r = 0; r < grid.Length; r++)
+                {
+                    for (int c = 0; c < grid[r].Length; c++)
+                    {
+                        if (grid[r][c] == '@')
+                        {
+                            if (IsDeletable(grid, r, c))
+                            {
+                                lastDeleted++;
+                                grid[r][c] = 'x';
+                            }
+                        }
+                    }
+                }
+                deletable += lastDeleted;
+            } while (lastDeleted > 0);
+            return "Part 2 Solution: " + deletable;
+        }
+
+        public static bool IsDeletable(char[][] grid, int r, int c)
+        {
+            List<char> adjacent = new List<char>();
+            if (r > 0)
+            {
+                adjacent.Add(grid[r - 1][c]); // Up
+            }
+            if (r < grid.Length - 1)
+            {
+                adjacent.Add(grid[r + 1][c]); // Down
+            }
+            if (c > 0)
+            {
+                adjacent.Add(grid[r][c - 1]); // Left
+            }
+            if (c < grid[r].Length - 1)
+            {
+                adjacent.Add(grid[r][c + 1]); // Right
+            }
+            if (r > 0 && c > 0)
+            {
+                adjacent.Add(grid[r - 1][c - 1]); // Up-Left
+            }
+            if (r > 0 && c < grid[r].Length - 1)
+            {
+                adjacent.Add(grid[r - 1][c + 1]); // Up-Right
+            }
+            if (r < grid.Length - 1 && c > 0)
+            {
+                adjacent.Add(grid[r + 1][c - 1]); // Down-Left
+            }
+            if (r < grid.Length - 1 && c < grid[r].Length - 1)
+            {
+                adjacent.Add(grid[r + 1][c + 1]); // Down-Right
+            }
+            if (adjacent.Where(x => x == '@').Count() > 3)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -3,8 +3,10 @@
     public static void Main(string[] args)
     {
         Console.WriteLine("Advent of Code 2025");
+
         AdventOfCode2025.Day1.Run();
         AdventOfCode2025.Day2.Run();
         AdventOfCode2025.Day3.Run();
+        AdventOfCode2025.Day4.Run();
     }
 }
