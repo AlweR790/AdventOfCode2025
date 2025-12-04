7b251f2026bf271f6576ea9e20c985d386179708
Jan Adamíra <jan.adamira@softgate.cz>
2025-12-04 15:42:36 +0100

    Changed selecting days

--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -3,10 +3,27 @@
     public static void Main(string[] args)
     {
         Console.WriteLine("Advent of Code 2025");
-
-        AdventOfCode2025.Day1.Run();
-        AdventOfCode2025.Day2.Run();
-        AdventOfCode2025.Day3.Run();
-        AdventOfCode2025.Day4.Run();
+        Console.WriteLine("-------------------");
+    DaySelect:
+        Console.WriteLine("Select day to run :");
+        switch (Console.ReadLine())
+        {
+            case "1":
+                AdventOfCode2025.Day1.Run();
+                break;
+            case "2":
+                AdventOfCode2025.Day2.Run();
+                break;
+            case "3":
+                AdventOfCode2025.Day3.Run();
+                break;
+            case "4":
+                AdventOfCode2025.Day4.Run();
+                break;
+            default:
+                Console.WriteLine("Invalid day selected. Please try again."); break;
+        }
+        Console.WriteLine("-------------------");
+        goto DaySelect; // :)) 292
     }
 }
