77aac5530cd3a90f20a28314535b4e6ccb8357b5
Jan Adamíra <jan.adamira@softgate.cz>
2025-12-08 10:28:16 +0100

    fixed day 8

--- AdventOfCode2025/Day8.cs
+++ AdventOfCode2025/Day8.cs
@@ -13,64 +13,138 @@
         public static string Part1()
         {
             List<Point> points = new List<Point>();
-            List<List<Point>> components = new List<List<Point>>();
+            List<List<int>> components = new List<List<int>>();
+            int c = 0;
             foreach (var line in File.ReadAllLines(InputFile))
             {
                 var coords = line.Split(',').Select(long.Parse).ToArray();
-                points.Add(new Point(coords[0], coords[1], coords[2]));
+                points.Add(new Point(c++, coords[0], coords[1], coords[2]));
             }
-            var distanceMap = new Dictionary<(Point, Point), long>();
+            var distanceMap = new Dictionary<(int, int), long>();
 
-            foreach (var point in points)
+            for (int i = 0; i < points.Count; i++)
             {
-                foreach (var otherPoint in points)
+                for (int j = i + 1; j < points.Count; j++)
                 {
-                    if (point != otherPoint)
-                    {
-                        distanceMap[(point, otherPoint)] = EuclideanDistance(point, otherPoint);
-                    }
+                    var p1 = points[i];
+                    var p2 = points[j];
+                    distanceMap[(p1.ID, p2.ID)] = EuclideanDistance(p1, p2);
                 }
             }
-            distanceMap.OrderBy(kv => kv.Value);
-            while (points.Any(p => !p.IsConnected))
+            var orderedMap = distanceMap.ToList().OrderBy(kv => kv.Value).ToList();
+            for (int i = 0; i < 1000; i++)
             {
-                while (distanceMap.Any())
+                var pair = orderedMap[i];
+                var p1 = points.Find(p => p.ID == pair.Key.Item1)!;
+                var p2 = points.Find(p => p.ID == pair.Key.Item2)!;
+
+                var comp1 = components.Find(comp => comp.Contains(p1.ID));
+                var comp2 = components.Find(comp => comp.Contains(p2.ID));
+
+                if (comp1 == null && comp2 == null)
                 {
-                    var kvp = distanceMap.First();
-                    var p1 = kvp.Key.Item1;
-                    var p2 = kvp.Key.Item2;
-                    if (!p1.IsConnected || !p2.IsConnected)
-                    {
-                        distanceMap.Remove(kvp.Key);
-                        if (!p1.IsConnected && !p2.IsConnected)
-                        {
-                            components.Add(new List<Point> { p1, p2 });
-                        }
-                        else if (p1.IsConnected && !p2.IsConnected)
-                        {
-                            var comp = components.First(c => c.Any(p => p.ID == p1.ID));
-                            comp.Add(p2);
-                        }
-                        else if (!p1.IsConnected && p2.IsConnected)
-                        {
-                            var comp = components.First(c => c.Any(p => p.ID == p2.ID));
-                            comp.Add(p1);
-                        }
-                        points.First(p => p.ID == p1.ID).IsConnected = true;
-                        points.First(p => p.ID == p2.ID).IsConnected = true;
-                        break;
-                    }
-                    distanceMap.Remove(kvp.Key);
+                    components.Add(new List<int> { p1.ID, p2.ID });
+                }
+                else if (comp1 != null && comp2 == null)
+                {
+                    if (!comp1.Contains(p2.ID))
+                        comp1.Add(p2.ID);
+                }
+                else if (comp1 == null && comp2 != null)
+                {
+                    if (!comp2.Contains(p1.ID))
+                        comp2.Add(p1.ID);
+                }
+                else if (comp1 != comp2)
+                {
+                    foreach (var id in comp2)
+                        if (!comp1.Contains(id))
+                            comp1.Add(id);
+
+                    components.Remove(comp2);
                 }
             }
-            components.OrderByDescending(c => c.Count);
+
+            components = components.OrderByDescending(c => c.Count).ToList();
             var sum = components[0].Count() * components[1].Count() * components[2].Count();
             return "Part 1 Solution: " + sum;
         }
         public static string Part2()
         {
-            // Implement Part 2 logic here
-            return "Part 2 Solution: ";
+            List<Point> points = new List<Point>();
+            List<List<int>> components = new List<List<int>>();
+            int c = 0;
+            foreach (var line in File.ReadAllLines(InputFile))
+            {
+                var coords = line.Split(',').Select(long.Parse).ToArray();
+                points.Add(new Point(c++, coords[0], coords[1], coords[2]));
+            }
+            var distanceMap = new Dictionary<(int, int), long>();
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                for (int j = i + 1; j < points.Count; j++)
+                {
+                    var p1 = points[i];
+                    var p2 = points[j];
+                    distanceMap[(p1.ID, p2.ID)] = EuclideanDistance(p1, p2);
+                }
+            }
+            var orderedMap = distanceMap.ToList().OrderBy(kv => kv.Value).ToList();
+            bool keepConnecting = true;
+            var lastPair = new ValueTuple<Point, Point>();
+            int y = 0;
+            while (keepConnecting)
+            {
+                var pair = orderedMap[y++];
+                var p1 = points.Find(p => p.ID == pair.Key.Item1)!;
+                var p2 = points.Find(p => p.ID == pair.Key.Item2)!;
+
+                var comp1 = components.Find(comp => comp.Contains(p1.ID));
+                var comp2 = components.Find(comp => comp.Contains(p2.ID));
+
+                bool merged = false;
+
+                if (comp1 == null && comp2 == null)
+                {
+                    components.Add(new List<int> { p1.ID, p2.ID });
+                    merged = true;
+                }
+                else if (comp1 != null && comp2 == null)
+                {
+                    if (!comp1.Contains(p2.ID))
+                    {
+                        comp1.Add(p2.ID);
+                        merged = true;
+                    }
+                }
+                else if (comp1 == null && comp2 != null)
+                {
+                    if (!comp2.Contains(p1.ID))
+                    {
+                        comp2.Add(p1.ID);
+                        merged = true;
+                    }
+                }
+                else if (comp1 != null && comp2 != null && comp1 != comp2)
+                {
+                    foreach (var id in comp2)
+                        if (!comp1.Contains(id))
+                            comp1.Add(id);
+
+                    components.Remove(comp2);
+                    merged = true;
+                }
+
+                if (merged)
+                    lastPair = (p1, p2);
+
+                if (components.Count == 1 && components[0].Count == points.Count)
+                    keepConnecting = false;
+            }
+
+            long prod = lastPair.Item1.X * lastPair.Item2.X;
+            return "Part 2 Solution: " + prod;
         }
 
         public class Point
@@ -80,8 +154,9 @@
             public long Y { get; set; }
             public long Z { get; set; }
             public bool IsConnected { get; set; } = false;
-            public Point(long x, long y, long z)
+            public Point(int id, long x, long y, long z)
             {
+                ID = id;
                 X = x;
                 Y = y;
                 Z = z;
